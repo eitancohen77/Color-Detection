7fb13d637ef47c876a2120573f9414aa91c62b3b
Eitan Cohen <eitancohen77@gmail.com>
2023-03-26 18:04:10 -0400

    Completed KNN model

--- data.py
+++ data.py
@@ -14,7 +14,10 @@ def random_color():
     blue = random.randint(0, 255)
     return f'rgb({str(red)}, {str(green)}, {str(blue)})'
 
-print(random_color())
+def get_rgb_values(color):
+    color = color.replace('rgb(', '').replace(')', '')
+    r, g, b = color.split(',')
+    return [int(r), int(g), int(b)]
 
 # Distance formula = sqrt((x2 - x1)^2 + (y2 - y1)^2). I would do the same but with a z vairable.
 
@@ -24,14 +27,20 @@ rand_color = random_color()
 target_colors = get_rgb_values(rand_color) # returns a list of 3 colors
 
 # Turn these colors into an array with 3 columns:
-target_color_df = pd.DataFrame([target_colors], names=['red', 'green', 'blue'])
+# We need to use a Series so we can subtract 1 value from the entire dataframe
+target_color_df = pd.Series({'red': target_colors[0], 'green': target_colors[1], 'blue': target_colors[2]})
 
-diff = colors - target_color_df
+diff = colors[['red', 'green', 'blue']] - target_color_df
 diff_sq = diff**2
 diff_sum = diff_sq.sum(axis = 1)
 dist = diff_sum**(1/2)
 dist_sorted = dist.sort_values()
 
-# In this case I am pulling the closest neighbor 
+print(dist_sorted)
+
+#In this case I am pulling the closest neighbor 
 k_closest = colors.loc[dist_sorted.index][:1]['color_name']
-probable_color = k_closest
+# I want it to return only the string name and not the Series
+probable_color = k_closest.iloc[0]
+
+print(probable_color)
